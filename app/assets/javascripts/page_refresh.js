1366a2cecaaba9036027381d997739d1fab5fe48
Brett Kinnamon <bkinn25@gmail.com>
2015-01-13 15:16:31 -0500

    fix #17

---
+++ app/assets/javascripts/page_refresh.js
@@ -0,0 +1,9 @@
+RELOAD_TIME = 15 * 60 * 1000  // 15 minutes
+
+$(document).on('page:load', function() {
+  setTimeout(reloadPage, RELOAD_TIME);
+});
+
+function reloadPage() {
+  window.location.href = document.location.href
+}
